d84bcef7c64679f301a4b0ea1c951c63958a325a
iamsahan <weerakkodyse@gmail.com>
2024-03-02 15:07:07 +0530

    last update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
                     <a href="#about" className="footer__link">About</a>
                 </li>
                 <li>
-                    <a href="#portfolio" className="footer__link">Projects</a>
+                    <a href="#portfolio\" className="footer__link">Projects</a>
                 </li>
                 <li>
                     <a href="#contact" className="footer__link">Contact Me</a>
